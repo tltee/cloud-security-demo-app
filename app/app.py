1a52d7f8b2afecb268cb89661493557798a6af84
bosen L <bl2961@nyu.edu>
2025-04-10 22:46:16 -0400

    modify

--- app/app.py
+++ app/app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', message='Hello, World!')
 
-#if __name__ == '__main__':
-#    app.run(debug=True)
+if __name__ == '__main__':
+   app.run(debug=True)
