f7bb3d96da316ad13eb6318a43649134c1e02d36
tltee <teetengloong@gmail.com>
2025-04-11 10:21:44 +0800

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,4 +3,4 @@ import os
 workers = int(os.environ.get("GUNICORN_PROCESSES", "2"))
 threads = int(os.environ.get("GUNICORN_THREADS", "4"))
 
-bind = os.environ.get("GUNICORN_BIND", "0.0.0.0:8000")
+bind = os.environ.get("GUNICORN_BIND", "0.0.0.0:8080")
